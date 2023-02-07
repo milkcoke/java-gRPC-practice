8d7a864fda58f5d4bbf02e524f9f85e7b7d0e166
milkcoke <mbh023@gmail.com>
2023-02-07 17:38:49 +0900

    feat: Add Bank Service

--- src/main/java/com/example/grpcpractice/server/BankService.java
+++ src/main/java/com/example/grpcpractice/server/BankService.java
@@ -4,10 +4,30 @@ import com.example.grpcpractice.proto.bank.Balance;
 import com.example.grpcpractice.proto.bank.BalanceCheckRequest;
 import com.example.grpcpractice.proto.bank.BankServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
+    private final BankRepository bankRepository;
+    // return type is void
+    // since there's stream observer
+
     @Override
     public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
-        super.getBalance(request, responseObserver);
+        // error channel : onError => 이때는 onComplete 호출할 필요 없음
+        // data channel: onNext => 완료되면 onComplete 호출
+
+        int reqAccountId = request.getAccountNumber();
+
+        int amount = bankRepository.getBalanceByAccountId(reqAccountId);
+
+        Balance balance = Balance.newBuilder()
+                        .setAmount(amount)
+                        .build();
+
+        responseObserver.onNext(balance);
+        responseObserver.onCompleted();
     }
 }
