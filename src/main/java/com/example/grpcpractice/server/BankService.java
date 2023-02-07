b1ef485b155d79ac8d2b5f411cc187c723a76130
milkcoke <mbh023@gmail.com>
2023-02-07 16:20:59 +0900

    build: Update build.gradle
    Update protoc version since it was not able to use.

--- src/main/java/com/example/grpcpractice/server/BankService.java
+++ src/main/java/com/example/grpcpractice/server/BankService.java
@@ -1,4 +1,13 @@
 package com.example.grpcpractice.server;
 
-public class BankService {
+import com.example.grpcpractice.proto.bank.Balance;
+import com.example.grpcpractice.proto.bank.BalanceCheckRequest;
+import com.example.grpcpractice.proto.bank.BankServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class BankService extends BankServiceGrpc.BankServiceImplBase {
+    @Override
+    public void getBalance(BalanceCheckRequest request, StreamObserver<Balance> responseObserver) {
+        super.getBalance(request, responseObserver);
+    }
 }
