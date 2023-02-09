c4260837d805236d24154e3ee68d02e219f6f9cd
milkcoke <mbh023@gmail.com>
2023-02-09 12:08:24 +0900

    test: Add Metadata test code

---
+++ src/test/java/com/example/grpcpractice/client/metadata/MetadataClientTest.java
@@ -0,0 +1,58 @@
+package com.example.grpcpractice.client.metadata;
+
+import com.example.grpcpractice.proto.bank.Balance;
+import com.example.grpcpractice.proto.bank.BalanceCheckRequest;
+import com.example.grpcpractice.proto.bank.BankServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.MetadataUtils;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MetadataClientTest {
+    private BankServiceGrpc.BankServiceBlockingStub blockingStub;
+
+    @DisplayName("클라이언트 토큰 검증 성공")
+    @Test
+    void successWithClientToken() {
+
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6443)
+                .intercept(MetadataUtils.newAttachHeadersInterceptor(ClientHeaders.getClientToken()))
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(2)
+                .build();
+
+        Balance balance = this.blockingStub.getBalance(balanceCheckRequest);
+
+        assertThat(balance.getAmount()).isEqualTo(2 * 10_000);
+    }
+
+    @DisplayName("클라이언트 토큰 검증 실패")
+    @Test
+    void failWithClientToken() {
+
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 6443)
+                .intercept(MetadataUtils.newAttachHeadersInterceptor(ClientHeadersInvalid.getClientToken()))
+                .usePlaintext()
+                .build();
+
+        this.blockingStub = BankServiceGrpc.newBlockingStub(managedChannel);
+
+
+        BalanceCheckRequest balanceCheckRequest = BalanceCheckRequest.newBuilder()
+                .setAccountNumber(3)
+                .build();
+
+        assertThrows(StatusRuntimeException.class, ()-> this.blockingStub.getBalance(balanceCheckRequest));
+    }
+
+}
